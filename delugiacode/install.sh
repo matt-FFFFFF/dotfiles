9614b2ed2c95c7356bcc2297bab28b1fe88c6d8e
Matt White <matt.white@microsoft.com>
2020-02-04 20:43:03 +0000

    Arch linux modifications

--- delugiacode/install.sh
+++ delugiacode/install.sh
@@ -35,20 +35,13 @@ done
 
 unset COMMANDS
 
-BASEURL="https://github.com"
-
 info 'Getting latest release'
-PAGE=$(curl --silent -L https://github.com/microsoft/cascadia-code/releases/latest)
-
-if [ ! "$PAGE" ]; then
-    fail 'Could not connect to GitHub'
-fi
 
 FONTS="Delugia.Nerd.Font.Complete"
 
 for f in $FONTS; do
     URL="https://github.com/adam7/delugia-code/releases/latest/download/"
-    URL="https://github.com${f}.ttf"
+    URL="${URL}${f}.ttf"
     OUT="$f.ttf"
     info "Downloading $URL to $OUT"
     curl -L $URL --output $OUT
@@ -57,17 +50,17 @@ done
 unset PAGE BASEURL PATTERN URL
 
 if [ ! -d /usr/local/share/fonts/truetype/delugia-code ]; then
-  info 'Creating directory: /usr/local/share/fonts/truetype/delugia-code'
-  sudo mkdir -pv /usr/local/share/fonts/truetype/cascadia-code
+  info 'Creating directory: /usr/local/share/fonts/delugia-code'
+  sudo mkdir -pv /usr/local/share/fonts/delugia-code
 fi
 
 info 'Installing fonts to /usr/share/local/fonts'
 
 for f in $FONTS; do
-  sudo cp $f.ttf /usr/local/share/fonts/truetype/delugia-code && rm $f.ttf
+  sudo cp $f.ttf /usr/local/share/fonts/delugia-code && rm $f.ttf
 done
 
 info 'Setting /usr/local/share/fonts/truetype/delugia-code to be world readable'
-sudo chmod -R o+rX /usr/local/share/fonts/truetype
+sudo chmod -R o+rX /usr/local/share/fonts
 
 success 'Finish delugiacode'
---
+++ kubernetes/final.zsh
@@ -0,0 +1,1 @@
+compdef k=kubectl
