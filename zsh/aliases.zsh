d4d9b5907eeb4bcbb7c117daa2fed325fb6e3c32
Matt White <matt.white@microsoft.com>
2020-02-14 13:55:36 +0000

    More arch updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,9 @@
 alias reload!='. ~/.zshrc'
-
 alias cls='clear' # Good 'ol Clear Screen command
+alias grep='grep --color=auto $*'
+alias ls='ls --color=auto $*'
+## Use a long listing format ##
+alias ll='ls -la $*'
+## Show hidden files ##
+alias l.='ls -d .* --color=auto $*'
+alias ll.='ls -ld .* --color=auto $*'
