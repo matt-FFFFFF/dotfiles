16c368649805ec5290c13e229925236c55d1f7b6
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-01-09 20:50:30 +0000

    feat: support for oneline

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -1,4 +1,8 @@
-source $(dirname $0)/p10k.config
+if [[ $TERM_PROGRAM == "WarpTerminal" ]]; then
+  source $(dirname $0)/p10k-oneline.config
+else
+  source $(dirname $0)/p10k.config
+fi
 
 if [ -f ~/powerlevel10k/powerlevel10k.zsh-theme ]; then
   source ~/powerlevel10k/powerlevel10k.zsh-theme
