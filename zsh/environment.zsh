cb3940a368f9deb0b450436d124e5c14e1426e7f
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-07-15 21:11:58 +0100

    fix: remove starship specific config

--- zsh/environment.zsh
+++ zsh/environment.zsh
@@ -1,5 +1,1 @@
-if [[ $TERM_PROGRAM == "WarpTerminal" ]]; then
-  export STARSHIP_CONFIG=~/.starship-warp.toml
-else
-  export STARSHIP_CONFIG=~/.starship.toml
-fi
+export STARSHIP_CONFIG=~/.starship.toml
